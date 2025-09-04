17d385dc7608f65edee6865bba90a6ca78340db3
Caven15 <cavenditch15@gmail.com>
2025-09-04 15:55:09 +0200

    Mise en place CRUD : personnages

--- app/services/menu.py
+++ app/services/menu.py
@@ -1,10 +1,20 @@
+from models.db.race import Race
 from .race_service import RaceService
+from .personnage_service import PersonnageService
 
 class Menu:
     def __init__(self, session):
         self.session = session
         self.race = RaceService(session)
+        self.personnage = PersonnageService(session)
         self.race.peupler()
     
     def test(self):
-        self.race.lister()
+        race_id = int(input("Id de la race"))
+        race = self.session.query(Race).filter_by(id=race_id).first()
+        perso = self.personnage.ajouter("Bilbon",race)
+        type(perso)
+        if perso is not None:
+            print(f"Personnage ajouté avec succès !!!")
+
+
---
+++ app/services/personnage_service.py
@@ -0,0 +1,48 @@
+from sqlalchemy import text
+from .db_tools import dbTools
+from models.db.personnage import Personnage
+
+class PersonnageService(dbTools):
+	"""Classe représentant la logique de gestion des personnages"""
+
+	def __init__(self,session):
+		super().__init__(session)
+
+	# Add => ORM
+	def ajouter(self, nom, race):
+		"""Ajoute un personnage avec méthode ORM"""
+		perso = Personnage(nom=nom, race=race)
+		self.session.add(perso)
+		self.commit()
+		return perso
+
+	# Read one
+	def chercher_par_id(self, id_):
+		"""Recherche un personnage par son id"""
+		return self.session.query(Personnage).filter_by(id=id_).first()
+
+	# READ one(toutes les quêts / anneaux)  => ORM 
+	def afficher_details(self, perso):
+		"""Affiche les détails d'un personnage"""
+		print(f"Nom : {perso.nom}")
+		print(f"Race : {perso.race.nom}")
+		# à complété avec d'autres tables....
+
+	# Udptate => ORM
+	def update_name(self, id, nouveau_nom):
+		"""Modifie le nom d'un personnage"""
+		p = self.chercher_par_id(id)
+		if p:
+			p.nom = nouveau_nom
+			self.commit()
+
+	# Delete => SQL BRUT
+	def supprimer(self, id_):
+		"""Supprimme un personnage via SQL BRUT"""
+		self.session.execute(
+			text("DELETE FROM personnage WHERE id = :{id}"),
+			{"id": id_}
+		)
+		self.commit()
+		print(f"Personnage id : {id_} supprimé !")
+
